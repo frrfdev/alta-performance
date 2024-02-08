2e5625e05e4f7eb34169dac50953a63dee3d7429
Felipe <feliperenanrf23@gmail.com>
2024-02-08 11:03:06 -0300

    test husky 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ContentSlider } from '@/components/ContentSlider/ContentSlider';
 
 const product = {
   id: '23726352376',
-  name: 'Product Name',
+  name: 'Relaxol A',
   headline: 'Product Description',
   profileSrc:
     'https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
